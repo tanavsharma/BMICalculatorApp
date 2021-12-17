ffba7d6d61a43c6be4e8d75d1880362ca77eb909
tanavsharma <sharma.tanav95@gmail.com>
2021-12-17 14:23:49 -0500

    saving information to firebase database, screen 1 done

--- BMICalculatorApp/AppDelegate.swift
+++ BMICalculatorApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
+        
+        
     }
 
     // MARK: UISceneSession Lifecycle
@@ -23,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
--- BMICalculatorApp/BMIScreen.swift
+++ BMICalculatorApp/BMIScreen.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class BMIScreen: UIViewController {
+    private let database = Database.database().reference()
 
     @IBOutlet weak var fullName: UITextField!
     @IBOutlet weak var age: UITextField!
@@ -29,12 +31,21 @@ class BMIScreen: UIViewController {
     @IBOutlet weak var genderET: UIView!
     @IBOutlet weak var fnEt: UIView!
     @IBOutlet weak var calcuView: UIView!
+    @IBOutlet weak var bmiResultView: UIView!
     
+    @IBOutlet weak var bmiResultLabel: UILabel!
     
+    var bmiCounter = 0
+    var bmiMax = ""
+    var bmiName = ""
     
     var mode = ""
 
     override func viewDidLoad() {
+        
+        var taskRef: DatabaseReference?
+        var refHandle: DatabaseHandle?
+        var numberOfBMIs: [String] = []
         
         super.viewDidLoad()
         
@@ -51,7 +62,7 @@ class BMIScreen: UIViewController {
         )
         
         gender.attributedPlaceholder = NSAttributedString(
-            string: " Enter your weight (e.x. Male)",
+            string: " Enter your Gender (e.x. Male)",
             attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
         )
         
@@ -66,6 +77,19 @@ class BMIScreen: UIViewController {
         createRound(weightView)
         createRound(heightView)
         createRound(calcuView)
+        createRound(bmiResultView)
+        
+        
+        taskRef = Database.database().reference(withPath: "BMIs")
+        refHandle = taskRef?.observe(DataEventType.value, with: { snapshot in
+            numberOfBMIs.removeAll()
+            for taskSnapshot in snapshot.children {
+                let nodeA = taskSnapshot as! DataSnapshot
+                let keyA = nodeA.key
+                numberOfBMIs.append(keyA)
+            }
+            self.bmiMax = numberOfBMIs.max()!
+        })
         
         
     }
@@ -84,6 +108,8 @@ class BMIScreen: UIViewController {
         imperial.setTitleColor(UIColor(red: 20/255, green: 117/255, blue: 255/255, alpha: 1), for:.normal)
         imperialBtnView.backgroundColor = UIColor(red: 39/255, green: 39/255, blue: 39/255, alpha: 1)
         
+        weightEt.text = ""
+        heightEt.text = ""
         
         
         
@@ -110,6 +136,9 @@ class BMIScreen: UIViewController {
         imperial.setTitleColor(.white, for:.normal)
         imperialBtnView.backgroundColor = .blue
         
+        weightEt.text = ""
+        heightEt.text = ""
+        
         
         
         weightEt.attributedPlaceholder = NSAttributedString(
@@ -127,19 +156,81 @@ class BMIScreen: UIViewController {
     
     @IBAction func calculateBMI(_ sender: Any) {
         
+        var bmiInformation: [String: String] = [:]
+        
         var bmi = 0.0
+        var bmiRes = ""
+        
+        let fn = fullName.text!
+        let umar = age.text!
+        let sex = gender.text!
         
         let weight:Double! = Double(weightEt.text!)
         let height:Double! = Double(heightEt.text!)
         
-        if(mode == "Imperial"){
-            print(mode)
-            bmi = (weight*703) / (height * height)
-        }else if(mode == "Metric"){
-            print(mode)
-            bmi = (weight) / (height * height)
+        if(weightEt.text!.isEmpty || heightEt.text!.isEmpty || fullName.text!.isEmpty || age.text!.isEmpty || gender.text!.isEmpty){
+            let alert = UIAlertController(title: "Fill Every Field", message: "You must fill out all fields to continue",
+                                          preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "Okay",
+                                          style: .default,
+                                          handler: nil))
+
+            self.present(alert, animated: true)
+            
+            
+        }else{
+            if(mode == "Imperial"){
+                
+                bmi = (weight*703) / (height * height)
+                bmi = round(bmi * 100) / 100.0
+            }else if(mode == "Metric"){
+               
+                bmi = (weight) / (height * height)
+                bmi = round(bmi * 100) / 100.0
+            }
+            
+            if(bmi <= 16){
+                bmiRes = "Severe Thinness" // blue
+                bmiResultView.backgroundColor = .blue
+            }else if(bmi >= 16 && bmi <= 17){
+                bmiRes = "Moderate Thinness" // blue
+                bmiResultView.backgroundColor = .blue
+            }else if(bmi >= 17 && bmi <= 18.5){
+                bmiRes = "Mild Thinness" // blue
+                bmiResultView.backgroundColor = .blue
+            }else if(bmi >= 18.5 && bmi <= 25){
+                bmiRes = "Normal Thinness"// green
+                bmiResultView.backgroundColor = .green
+            }else if(bmi >= 25 && bmi <= 30){
+                bmiRes = "Overweight" //yellow
+                bmiResultView.backgroundColor = .yellow
+                bmiResultLabel.textColor = .black
+            }else if(bmi >= 30 && bmi <= 35){
+                bmiRes = "Obese Class 1" // orange
+                bmiResultView.backgroundColor = .orange
+            }else if(bmi >= 35 && bmi <= 40){
+                bmiRes = "Obese Class 2" // orange
+                bmiResultView.backgroundColor = .orange
+            }else if(bmi > 40){
+                bmiRes = "Obese Class 3" // red
+                bmiResultView.backgroundColor = .red
+            }
+            bmiResultLabel.text = "Your BMI is \(bmi). You are \(bmiRes)"
+            bmiCounter = Int(bmiMax)! + 1
+            
+            bmiInformation["Name"] = fn
+            bmiInformation["Age"] = umar
+            bmiInformation["Gender"] = sex
+            bmiInformation["BMI Value"] = String(bmi)
+            bmiInformation["BMI Result"] = bmiRes
+            
+            
+
+            database.child("BMIs").child("\(bmiCounter)").setValue(bmiInformation)
+            
+            
         }
-        print(bmi)
     }
     
     
